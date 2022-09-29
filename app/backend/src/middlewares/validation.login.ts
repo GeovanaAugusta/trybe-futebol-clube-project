f7a4f4568f9797690439680b94433076d81d10e6
Geovana Augusta <geovanaag@hotmail.com>
2022-09-29 14:42:56 -0300

    feat: validations user

---
+++ app/backend/src/middlewares/validation.login.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validationLogin = {
+  validateLogin(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400)
+        .json({ message: 'All fields must be filled' });
+    }
+
+    if (!email || !password || password === '') {
+      res.status(400).json({ message: 'All fields must be filled' });
+    }
+
+    next();
+  },
+};
+
+export default validationLogin;
--- app/backend/src/routes/user.route.ts
+++ app/backend/src/routes/user.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+// import validationLogin from '../middlewares/validation.login';
 
 const userRouter = Router();
 
