8a295a91829b56d31938a9574afffcefa7a2601d
Geovana Augusta <geovanaag@hotmail.com>
2022-09-29 15:37:58 -0300

    feat: validation fields POST /login

--- app/backend/src/middlewares/validation.login.ts
+++ app/backend/src/middlewares/validation.login.ts
@@ -3,14 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 const validationLogin = {
   validateLogin(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
-    // console.log(email, password);
+    console.log(email, password);
 
-    const emailRegex = /\S+@\S+\.\S+/;
+    // const emailRegex = /\S+@\S+\.\S+/;
 
-    if (!emailRegex.test(email)) {
-      return res.status(400)
-        .json({ message: 'All fields must be filled' });
-    }
+    // if (!emailRegex.test(email)) {
+    //   return res.status(400)
+    //     .json({ message: 'All fields must be filled' });
+    // }
 
     if (!email || !password || password === '') {
       res.status(400).json({ message: 'All fields must be filled' });
--- app/backend/src/routes/user.route.ts
+++ app/backend/src/routes/user.route.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
+import validationLogin from '../middlewares/validation.login';
 import UserController from '../controllers/user.controller';
-
-// console.log(validationLogin.validateLogin);
 
 const userRouter = Router();
 
 const userController = new UserController();
 
-userRouter.route('/').post(userController.checkUser);
+userRouter.route('/').post(validationLogin.validateLogin, userController.checkUser);
 
 export default userRouter;
